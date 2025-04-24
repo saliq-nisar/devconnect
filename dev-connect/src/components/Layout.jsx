5bf6e5f9da4b293025c622b47aab5d0fd7c9d0e2
Saliq Nisar <msalik909@gmail.com>
2025-04-24 16:25:58 +0530

    base

---
+++ dev-connect/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Navbar />
+      <main style={{ flex: 1, padding: "1rem" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
