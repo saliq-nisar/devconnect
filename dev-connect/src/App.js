3a49e35dcab9e5643b00c1c734cba360267c42c0
Saliq Nisar <msalik909@gmail.com>
2025-04-24 16:36:54 +0530

    login and sign up skeleton

--- dev-connect/src/App.js
+++ dev-connect/src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
+      <Routes>
+       
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
     </Router>
   );
 };
