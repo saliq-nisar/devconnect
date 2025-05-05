5e66280b88a461e1635701967326f84783d7c188
Saliq Nisar <msalik909@gmail.com>
2025-05-05 12:24:23 +0530

    ..

--- dev-connect/src/components/Navbar.jsx
+++ dev-connect/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <nav style={{ padding: "1rem", backgroundColor: "#333", color: "#fff" }}>
       <a href="/home" style={{ color: "#fff" }}>Home</a>
@@ -13,4 +12,4 @@ const Navbar = (props) => {
 };
  
 
-export default React.memo(Navbar);
+export default Navbar;
